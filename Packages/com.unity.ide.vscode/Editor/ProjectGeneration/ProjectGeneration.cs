f09960f098d20b83b9b8afe0bea36c979579e744
Gonçalo Vasconcelos <goncalo.vasconcelos@realgames.pt>
2021-10-28 18:25:09 +0100

    Fix paths for source files during project generation

--- Packages/com.unity.ide.vscode/Editor/ProjectGeneration/ProjectGeneration.cs
+++ Packages/com.unity.ide.vscode/Editor/ProjectGeneration/ProjectGeneration.cs
@@ -575,7 +575,7 @@ namespace VSCodeEditor
                 .Select(path => MakeAbsolutePath(path, ProjectDirectory).NormalizePath())
                 .ToArray());
         }
-        
+
         private static string MakeAbsolutePath(string path, string projectDirectory)
         {
             return Path.IsPathRooted(path) ? path : Path.Combine(projectDirectory, path);
@@ -739,7 +739,7 @@ namespace VSCodeEditor
             file = file.NormalizePath();
             var path = SkipPathPrefix(file, projectDir);
 
-            var packageInfo = m_AssemblyNameProvider.FindForAssetPath(path.NormalizePath());
+            var packageInfo = m_AssemblyNameProvider.FindForAssetPath(path.Replace('\\', '/'));
             if (packageInfo != null)
             {
                 // We have to normalize the path, because the PackageManagerRemapper assumes
